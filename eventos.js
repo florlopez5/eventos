596d4c0e119301d88d84ac775988c3cabe96d01d
florlopez5 <flolom21@gmail.com>
2023-08-07 00:58:14 -0300

    Se sube solución

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
+let contador = 0;
 document.addEventListener("DOMContentLoaded", function() {
-    const elDiv = document.getElementById("elDiv");
-    elDiv.addEventListener("click", function() {
-      alert("Hola! Soy el div");
-    });
-  });
+  document.getElementById("miBoton").addEventListener("click", function() {
+    contador++;
+    if (contador % 2) {
+      alert("Hola");
+    } else {
+      alert("Hola soy el div");
+    }
+  }
+  )
+});
